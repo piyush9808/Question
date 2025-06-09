24f1d32a785707e705655048fcac0627e526380d
Piyush Kumar <piyushkumarsings@gmail.com>
2025-06-09 17:35:15 +0530

    AI change

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ function page() {
 
     <div className='p-10 bg-black'>
       <h2 className='font-bold text-2xl text-white'>Dashboard</h2>
-      <h2 className='text-gray-500'>Start Your Mockup Ai Interview</h2>
+      <h2 className='text-gray-500'>Start Your Mockup AI Interview</h2>
       <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
         <AddNewInterview />
       </div>
